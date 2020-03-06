1e53d12d794531ab4b4ea013fe51f5cbca33c743
Nirjhar Debnath <nirjhar@Nirjhars-MacBook-Air.local>
2020-03-06 10:05:07 +0530

    Updates

---
+++ precache-manifest.3d8801fdf6ddd0d17e2cb75773e274ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4df721e66ba34e26a32a2aea1b47f25",
+    "url": "/task-list/index.html"
+  },
+  {
+    "revision": "156aa477eaa3763bcaed",
+    "url": "/task-list/static/css/main.7641ce51.chunk.css"
+  },
+  {
+    "revision": "501516d6d779826ea816",
+    "url": "/task-list/static/js/2.50c93b05.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/task-list/static/js/2.50c93b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156aa477eaa3763bcaed",
+    "url": "/task-list/static/js/main.a6cc87fb.chunk.js"
+  },
+  {
+    "revision": "47e8b284f9bbc68edcca",
+    "url": "/task-list/static/js/runtime-main.547966f2.js"
+  }
+]);
